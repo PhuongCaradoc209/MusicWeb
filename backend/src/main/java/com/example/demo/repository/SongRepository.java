1f910419a72785c629f98356adb00b3bed988e14
= <phuongnguyen20904@gmail.com>
2025-02-23 21:21:46 +0700

    Feat: Add AlbumRepository and ArtistRepository for album and artist data management

--- backend/src/main/java/com/example/demo/repository/SongRepository.java
+++ backend/src/main/java/com/example/demo/repository/SongRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SongRepository extends JpaRepository<Song, Long> {
+    Optional<Song> findBySpotifyId(String spotifyId);
 }
