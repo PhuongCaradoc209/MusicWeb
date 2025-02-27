a071689790bdf14ee49f0b0471c1e1ef46961c6d
= <phuongnguyen20904@gmail.com>
2025-02-27 20:08:22 +0700

    Feat: Integrate Spotify Web Playback SDK and add player routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-// App.js
 import { BrowserRouter as Router, Navigate, useRoutes } from "react-router-dom";
+import { useEffect } from "react"; // Import useEffect
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/LoginSignUpPage/Login";
 import SignUp from "./pages/LoginSignUpPage/SignUp";
@@ -8,6 +8,8 @@ import SongPage from "./pages/HomePage/SongPage";
 import { AuthProvider } from "./helpers/AuthorProvider";
 import Top_50_page from "./pages/HomePage/Top_50_page";
 import Songs from "./pages/HomePage/Songs";
+import PlayerPage from "./pages/PlayerPage";
+import Callback from "./pages/LoginSignUpPage/Callback";
 
 const homeRoutes = [
   { path: "browsePage", element: <BrowsePage /> },
@@ -26,11 +28,32 @@ function AppRoutes() {
     { path: "/", element: <Navigate to="/browsePage" replace /> },
     { path: "/", element: <HomePage />, children: homeRoutes },
     { path: "/login", element: <Login /> },
-    { path: "/signup", element: <SignUp /> }
+    { path: "/signup", element: <SignUp /> },
+    { path: "/callback", element: <Callback /> },
+    { path: "/player/:id", element: <PlayerPage /> }
   ]);
 }
 
 export default function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+    script.onload = () => {
+      console.log("Spotify Web Playback SDK đã tải xong!");
+
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        console.log("SDK sẵn sàng!", window.Spotify);
+      };
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <AuthProvider>
       <Router>
