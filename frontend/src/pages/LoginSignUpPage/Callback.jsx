c804f44ab501d603fd40a3b1bed398634fd7c22b
= <phuongnguyen20904@gmail.com>
2025-02-27 20:08:39 +0700

    Feat: Add Callback component to handle Spotify login redirection and token storage

---
+++ frontend/src/pages/LoginSignUpPage/Callback.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function Callback() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get("code");
+        console.log(code);
+
+        if (code) {
+            axios.get(`http://localhost:8080/api/spotify/callback?code=${code}`)
+                .then((res) => {
+                    localStorage.setItem("spotifyAccessToken", res.data.accessToken);
+                    console.log(res.data.accessToken);
+
+                    navigate("/");
+                })
+                .catch((error) => {
+                    console.error("Spotify callback error:", error);
+                });
+        }
+    }, [navigate]);
+
+    return <p>Processing Spotify login...</p>;
+}
+
+export default Callback;
